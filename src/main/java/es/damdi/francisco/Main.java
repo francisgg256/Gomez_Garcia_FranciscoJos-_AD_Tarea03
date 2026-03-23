1b033f2ba87284850bf508665488f08e2249c2da
francisgg256 <fjoseprohias@gmail.com>
2026-03-23 12:49:00 +0100

    Menu hecho

--- src/main/java/es/damdi/francisco/Main.java
+++ src/main/java/es/damdi/francisco/Main.java
@@ -1,14 +1,37 @@
 package es.damdi.francisco;
 
 import es.damdi.francisco.operaciones.GestorOperaciones;
+import org.iesalandalus.programacion.utilidades.Entrada;
 
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Iniciando programa...");
         GestorOperaciones gestor = new GestorOperaciones();
-        gestor.altaEspecialidad("PR", "Programación");
+        int opcion;
 
-        System.out.println("Fin del programa.");
+        do {
+            System.out.println("\n**************************************");
+            System.out.println("     GESTIÓN CENTRO PROFESORES");
+            System.out.println("**************************************");
+            System.out.println("1.- Alta de Especialidad");
+            System.out.println("0.- Salir");
+            System.out.print("Selecciona una opción: ");
+
+            opcion = Entrada.entero();
+
+            switch (opcion) {
+                case 1:
+                    System.out.print("Introduce el código (ej. IN): ");
+                    String id = Entrada.cadena();
+                    System.out.print("Introduce el nombre: ");
+                    String nombre = Entrada.cadena();
+                    gestor.altaEspecialidad(id, nombre);
+                    break;
+                case 0:
+                    System.out.println("Cerrando aplicación...");
+                    break;
+                default:
+                    System.out.println("Opción no válida.");
+            }
+        } while (opcion != 0);
     }
 }
