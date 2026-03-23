58244b4ca8d96cb863f85a6e0994b31bfb52b001
francisgg256 <fjoseprohias@gmail.com>
2026-03-23 13:47:03 +0100

    Menu hecho

--- src/main/java/es/damdi/francisco/operaciones/GestorOperaciones.java
+++ src/main/java/es/damdi/francisco/operaciones/GestorOperaciones.java
@@ -1,9 +1,11 @@
 package es.damdi.francisco.operaciones;
 
 import es.damdi.francisco.dominio.Especialidad;
+import es.damdi.francisco.dominio.Profesor;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import es.damdi.francisco.utilidades.HibernateUtil;
+import java.util.List;
 
 public class GestorOperaciones {
     public void altaEspecialidad(String id, String nombre) {
@@ -22,6 +24,23 @@ public class GestorOperaciones {
                 tx.rollback();
             }
             System.out.println("Error al guardar la especialidad: " + e.getMessage());
+        } finally {
+            sesion.close();
+        }
+    }
+
+    public void listarProfesores() {
+        Session sesion = HibernateUtil.getSessionFactory().openSession();
+        try {
+            List<Profesor> lista = sesion.createQuery("from Profesor", Profesor.class).getResultList();
+
+            System.out.println("\n--- LISTADO DE PROFESORES ---");
+            for (Profesor p : lista) {
+                System.out.println("ID: " + p.getCodProf() + " | Nombre: " + p.getNombreApe() +
+                        " | Especialidad: " + (p.getEspecialidad() != null ? p.getEspecialidad().getNombre() : "N/A"));
+            }
+        } catch (Exception e) {
+            System.out.println("Error al listar: " + e.getMessage());
         } finally {
             sesion.close();
         }
